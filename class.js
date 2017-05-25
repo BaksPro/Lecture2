b4665ef82526c134caace6099aec1f395014048f
BaksPro <baksspro@gmail.com>
2017-05-25 21:39:48 +0300

    Final commit

--- class.js
+++ class.js
@@ -1,69 +1,58 @@
-class Fighter{
-    constructor( name, power = 10, health = 100) {
-        this.name = name ;
-        this.power = power;
-        this.health = health;
-    }
-    setDamage(damage) {
-        this.health -= damage ;
-     
-    }
-    
-    hit(enemy,point) {
-       
-        this.point = point;
-        this.damage = this.point * enemy.power;
-        enemy.setDamage(this.damage)
-           console.log(`${enemy.name} bit ${this.damage} damage  to ${this.name}. ${this.name} health: ${enemy.health}`)
-        
-    }
+class Fighter {
+   constructor(name, power = 10, health = 100) {
+       this.name = name;
+       this.power = power;
+       this.health = health;
+   }
 
+   setDamage(damage) {
+       this.health -= damage;
+   } 
+
+   hit(enemy, point) {       
+       let damage = point * enemy.power;
+       enemy.setDamage(damage)
+       console.log(`${this.name} bit ${damage} damage  to ${enemy.name}. ${enemy.name} health: ${enemy.health}`)
+   } 
 }
-
-class ImprovedFighter extends Fighter {
-    
-    
-    doubleHit(enemy, point) {
-        this.point = point * 2; 
-        super.hit(enemy, this.point  )
-    }
-    
-}
-    
-
+ 
+class ImprovedFighter extends Fighter { 
    
-
-
-let fighter = new Fighter( "Baks", 10, 2000);
+   doubleHit(enemy, point) {
+       super.hit(enemy, point * 2)
+   } 
+} 
+ 
+let fighter = new Fighter("Baks", 10, 2000);
 let improvedFighter = new ImprovedFighter("Axe", 10, 2000)
+  
+function fight(defaultFidhter, upgradeFighter, ...points) {
+    let round = 1;
 
-
-
-
-
-function fight(defaultFidhter,upgradeFighter,...points) {
-
-     points.forEach((point,i) => {
+    for (let point of points) {
+        console.log(`_______________________Round ${round++} Start:___________________________________`)
+ 
+        defaultFidhter.hit(upgradeFighter, point);
+            if (upgradeFighter.health < 0) {
+                return console.log(`${upgradeFighter.name} lose. ${defaultFidhter.name} win!!!Congratulations!`)
+            }
         
-      
-         if( i % 2 !== 0) {                      
-               ; 
-            defaultFidhter.hit(upgradeFighter,point);
-            if( upgradeFighter.health < 0) return console.log( "lose") 
-            
-         } else {
-             
-             upgradeFighter.doubleHit(defaultFidhter,point);
-             if( defaultFidhter.health < 0) return  console.log( "lose");
-                
-         }
-     }) 
+        upgradeFighter.doubleHit(defaultFidhter, point);
+            if (defaultFidhter.health < 0) {
+                return console.log(`${upgradeFighter.name} lose. ${thdefaultFidhteris.name} win!!!Congratulations!`)
+            }
 
-     
-
-
+       console.log(`_______________________Round ${round++} End:_____________________________________ `)
+   }
 }
-
-fight(fighter,improvedFighter,10,50,50,60,60,67,67)
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+fight(fighter, improvedFighter, 10, 10, 600)
+ 
+ 
+ 
+ 
